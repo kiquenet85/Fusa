6e99f50cce87d1131b96cc31120acd72b4eef2d8
kiquenet85 <kiquenet85@gmail.com>
2014-09-06 12:14:03 -0500

    Usando Intent implícito para mostrar ubicación! Intenciones comunes en apps: http://bit.ly/1u9yv6D

--- src/com/example/zonamigosfusa/MainActivity.java
+++ src/com/example/zonamigosfusa/MainActivity.java
@@ -4,11 +4,14 @@
 package com.example.zonamigosfusa;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.zonamigosfusa.otro.SecondActivity;
 
@@ -19,16 +22,18 @@ import com.example.zonamigosfusa.otro.SecondActivity;
 public class MainActivity extends ActionBarActivity {
 
 	//-- Este botón existe en el layout activity_main.
-	private Button segundaActividad; 
+	private Button btnSegundaActividad, btnMostrarUbicacion;
+	private String LOG_TAG=this.getClass().getSimpleName(); 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
 		//-- Se pueden utilizar las vistas del layout activity_main de la siguiente manera:
-		segundaActividad=(Button)findViewById(R.id.button1);
+		btnSegundaActividad=(Button)findViewById(R.id.button1);
+		btnMostrarUbicacion=(Button)findViewById(R.id.button2);
 		//-- Ejemplo de un evento para un botón.
-		segundaActividad.setOnClickListener(new OnClickListener() {
+		btnSegundaActividad.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
@@ -40,6 +45,31 @@ public class MainActivity extends ActionBarActivity {
 			}
 		});
 		
-		
+		btnMostrarUbicacion.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//Es posible también llamar la siguiente actividad de dos formas:
+				showLocationInMap();
+			}
+		});
 	}
+		
+	public void showLocationInMap() {
+		Uri uri = Uri
+				.parse("geo:4.334858,-74.350432?q=Fusagasuga+Cundinamarca,+Colombia");
+        //Intent intent = new Intent(android.content.Intent.ACTION_VIEW, uri);
+		Intent intent = new Intent();
+		intent.setData(uri);
+		intent.setAction(android.content.Intent.ACTION_VIEW);
+		Toast.makeText(this, uri.toString(), Toast.LENGTH_SHORT).show();
+		if (intent.resolveActivity(this.getPackageManager()) != null) {
+			startActivity(intent);
+		} else {
+			String msg = "Is not possible to see location in MAP, there are not available apps";
+			Log.d(LOG_TAG, msg);
+			Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+		}
+	}
+	
 }
