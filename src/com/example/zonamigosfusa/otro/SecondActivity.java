82d6beaf8d6b97451ead16745c39413893690ff3
kiquenet85 <kiquenet85@gmail.com>
2014-09-13 00:57:32 -0500

    Crear una lista en la segunda actividad al cambiar dinámicamente un fragmento (datos quemados)

--- src/com/example/zonamigosfusa/otro/SecondActivity.java
+++ src/com/example/zonamigosfusa/otro/SecondActivity.java
@@ -1,12 +1,18 @@
 package com.example.zonamigosfusa.otro;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,11 +20,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.zonamigosfusa.R;
 
 public class SecondActivity extends ActionBarActivity {
 	
+	private PlaceholderFragment firstFragment;
+	private SecondFragment secondFragment;
 	private String tag = "FusaEnterpriseLog";
 	
 	@Override
@@ -27,8 +39,9 @@ public class SecondActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_second);
 		actionBar();
 		if (savedInstanceState == null) {
+			firstFragment= new PlaceholderFragment();
 			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+					.add(R.id.container,firstFragment ).commit();
 		}
 	}
 
@@ -55,6 +68,30 @@ public class SecondActivity extends ActionBarActivity {
 	private void callPreferences() {
 		Intent intent=new Intent(this,SettingsActivity.class);
         startActivity(intent);
+	}
+	
+	public void changeFragment(View v){
+		Toast.makeText(this, "changing Fragment dinamically", Toast.LENGTH_LONG).show();
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		FragmentTransaction fragmentTransaction = 
+				fragmentManager.beginTransaction(); 
+		
+		
+			fragmentTransaction.remove(firstFragment);
+			
+		
+		 fragmentTransaction.addToBackStack(null);
+			fragmentTransaction.commit();
+			
+			//--Adding new Fragment
+			if (secondFragment==null){
+				secondFragment=new SecondFragment();
+			}
+			fragmentManager = getSupportFragmentManager();
+			fragmentTransaction =fragmentManager.beginTransaction(); 
+     		fragmentTransaction.add(R.id.container,secondFragment, "NewFragment");
+         	fragmentTransaction.addToBackStack(null);
+			fragmentTransaction.commit();
 	}
 
 		// Called before subsequent visible lifetimes
@@ -149,6 +186,54 @@ public class SecondActivity extends ActionBarActivity {
 			View rootView = inflater.inflate(R.layout.fragment_second,
 					container, false);
 			return rootView;
+		}
+	}
+	
+	public static class SecondFragment extends Fragment {
+
+		private ArrayAdapter listAdapter;
+		
+		public SecondFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_second_list,
+					container, false);
+
+            String[] contacts ={
+                "Nestor Diazgranados . Bogota",
+                "Carlos Umaña - Zipaquira",
+                "Diana Padilla - Silvania",
+                "Angela Beltran - Fusagasuga",
+                "Miguel Angel - Fusagasuga",
+                "Juan Camilo - Fusagasuga",
+                "Juana de Arco - Fusagasuga",
+                "Gomela chicomosa- Fusagasuga",
+                "la fulana - Fusagasuga",
+                "El come mocos :(- Fusagasuga",
+                
+        };
+            //--This is to be able to call the clear method and add method on this list collection.
+            List<String> contactsAsList = new ArrayList<String> (Arrays.asList(contacts));
+            
+            ListView listView=(ListView)rootView.findViewById(R.id.listView1);
+            listAdapter=new ArrayAdapter(getActivity(),R.layout.list_item_contact_textview,R.id.list_item_contact_textview,contactsAsList);
+            listView.setAdapter(listAdapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String forecast=(String)listAdapter.getItem(position);
+                    Toast.makeText(view.getContext(),forecast,Toast.LENGTH_SHORT).show();
+                    
+                    /*Intent intent= new Intent(getActivity(),DetailActivity.class);
+                    intent.putExtra(Intent.EXTRA_TEXT,forecast);
+                    startActivity(intent);*/
+                }
+            });
+            return rootView;
+       
 		}
 	}
 }
